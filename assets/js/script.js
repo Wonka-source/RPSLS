414b09fcce99996b154339b9a651a63953cc1615
Wonka-source <dan.eff117@gmail.com>
2022-08-06 16:58:55 +0000

    add some boiler plate functions to .js file

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+const options = document.querySelectorAll(.btn--options)
+
+
+
+
+
+
+
+
+function playGame(){
+
+}
+
+
+
+function getComputerChoice(){
+
+}
+
+
+
+function getWinner(){
+
+}
+
+function showMoves(){
+
+}
+
+
+
+function restartGame () {
+    
+}
